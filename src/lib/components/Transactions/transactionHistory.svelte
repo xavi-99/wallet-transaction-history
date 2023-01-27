c85a0d267ce0279ec4782fd8ba3d0d4407ae6b3e
Xavi Navarro <xnavarroiguacel@gmail.com>
2023-01-27 11:32:39 +0100

    feat: Basic Transaction History Table component

--- src/lib/components/Transactions/transactionHistory.svelte
+++ src/lib/components/Transactions/transactionHistory.svelte
@@ -1,5 +1,23 @@
 <script>
-    import Table from "../shared/table.svelte"
+	import { each } from "svelte/internal";
+
+    const tableHeaders = ['Header 1', 'Header 2', 'Header 3'];
+    const transactionData = [{
+        "field1" : "field1",
+        "field2" : "field2",
+        "field3" : "field3",
+        "field4" : "field4"
+    }]
+
 </script>
 
-<Table></Table>
+<table>
+    <thead>
+        {#each tableHeaders as header}
+            <th>{header}</th>
+        {/each}
+    </thead>
+    <tbody>
+
+    </tbody>
+</table>
