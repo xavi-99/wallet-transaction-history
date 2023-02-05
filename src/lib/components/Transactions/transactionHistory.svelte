5c409e1380a6963e7eeab2e75f919c00e6268d28
Xavi Navarro <xnavarroiguacel@gmail.com>
2023-02-05 08:39:18 +0100

    feat: edit button on talbe, non styled , event handler for click

--- src/lib/components/Transactions/transactionHistory.svelte
+++ src/lib/components/Transactions/transactionHistory.svelte
@@ -1,41 +1,48 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-
+	import editButton from '$lib/images/edit.svg';
 	import {
 		Alchemy,
 		AssetTransfersCategory,
 		Network,
-		type AssetTransfersResponse,
+		type AssetTransfersResponse
 		// type AssetTransfersResult
 	} from 'alchemy-sdk';
 
 	let transactionsHeaders: string[] = [];
 	// let transactionsData: AssetTransfersResult[] = [];
-	type AssetTransfersResult ={
-		to: string|null;
-		from: string|null;
-		asset: string|null;
+	type AssetTransfersResult = {
+		to: string | null;
+		from: string | null;
+		asset: string | null;
 		value: number | null;
-	}
+	};
 	let transactionsData: AssetTransfersResult[] = [];
 
-	const transactionFieldsToDisplay = ["from", "to", "value", "asset"]
+	const transactionFieldsToDisplay = ['from', 'to', 'value', 'asset'];
 
 	const config = {
 		apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
 		network: Network.ETH_GOERLI
 	};
-	
+
+	const handleEditClick = (transaction: AssetTransfersResult) => {
+	};
+
 	onMount(async () => {
 		const alchemy = new Alchemy(config);
 		const transactions: AssetTransfersResponse = await alchemy.core.getAssetTransfers({
 			category: [AssetTransfersCategory.ERC20, AssetTransfersCategory.ERC721]
 		});
 		if (transactions && transactions.transfers) {
-			transactionsHeaders = transactionFieldsToDisplay
-			transactionsData = transactions.transfers.map(({from,to,value,asset}) => ({from, to, value,asset}));
+			transactionsHeaders = transactionFieldsToDisplay;
+			transactionsData = transactions.transfers.map(({ from, to, value, asset }) => ({
+				from,
+				to,
+				value,
+				asset
+			}));
 		}
-		debugger;
 	});
 </script>
 
@@ -51,7 +58,20 @@
 				<tr>
 					{#each Object.entries(transaction) as [key, value]}
 						<td>
-							{value}
+							{#if key === 'from' || key === 'to'}
+								<div class="flex">
+									<div>
+										<button on:click={() => handleEditClick(transaction)} class="edit-tx-btn">
+											<img src={editButton} alt="Edit your transaction wallet address"/>
+										</button>
+									</div>
+									<div>
+										{value}
+									</div>
+								</div>
+							{:else}
+								{value}
+							{/if}
 						</td>
 					{/each}
 				</tr>
@@ -59,3 +79,17 @@
 		</tbody><tbody />
 	</table>
 </figure>
+
+<style>
+	.flex {
+		display: flex;
+		flex-direction: columns;
+		align-items: center;
+	}
+	.flex div {
+		padding: 0 10px;
+	}
+	.flex div button {
+		margin-bottom: 0px;
+	}
+</style>
